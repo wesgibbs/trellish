97c8a8e989e13cbbc130eeaae8f8c43b3b0db20e
Wes Gibbs <wesgibbs@gmail.com>
2012-11-02 08:59:38 -0700

    Give useful error message on github connect fail

--- lib/trellish/git.rb
+++ lib/trellish/git.rb
@@ -13,15 +13,20 @@ module Trellish
         faraday.request :url_encoded
         faraday.adapter ::Faraday.default_adapter
       end
-      response = conn.post do |req|
-        req.url "/repos/#{git_repository_owner}/#{git_repository_name}/pulls"
-        req.headers['Content-Type'] = 'application/json'
-        req.headers['Authorization'] = "token #{Trellish.config[:github_oauth_token]}"
-        req.body = {
-          title: @card.name,
-          base: Trellish.config[:git_base_branch],
-          head: "#{git_repository_owner}:#{current_git_branch}"
-        }.to_json
+      begin
+        response = conn.post do |req|
+          req.url "/repos/#{git_repository_owner}/#{git_repository_name}/pulls"
+          req.headers['Content-Type'] = 'application/json'
+          req.headers['Authorization'] = "token #{Trellish.config[:github_oauth_token]}"
+          req.body = {
+            title: @card.name,
+            base: Trellish.config[:git_base_branch],
+            head: "#{git_repository_owner}:#{current_git_branch}"
+          }.to_json
+        end
+      rescue Faraday::Error::ConnectionFailed => e
+        Trellish.logger.error "Failed to connect to Github. Please check your github_oauth_token parameter in trellish.yml, or regenerate it if you continue to have problems. Original error: #{e.message}"
+        exit
       end
       @github_pull_request_url = JSON.parse(response.body)["html_url"]
     end
